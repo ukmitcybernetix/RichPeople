89fb476301f3e66d3a6fab0990d073ec217d7aca
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-09-10 12:34:35 +0700

    Update Class People

--- app/src/main/java/com/zendev/daftarorangkaya/model/People.kt
+++ app/src/main/java/com/zendev/daftarorangkaya/model/People.kt
@@ -1,2 +1,12 @@
 package com.zendev.daftarorangkaya.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class People(
+    var name: String? = null,
+    var founder: String? = null,
+    var description: String? = null,
+    var photo: String? = null
+) : Parcelable
